35835183edc08830a1c60c014c70337fcbc78624
Baiketli Dijo <baiketlid@gmail.com>
2024-11-09 21:17:59 +0200

    Scaffolded the entity and ef model.

---
+++ Data/BlazorWebAppMoviesContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BlazorWebAppMovies.Models;
+
+namespace BlazorWebAppMovies.Data
+{
+    public class BlazorWebAppMoviesContext : DbContext
+    {
+        public BlazorWebAppMoviesContext (DbContextOptions<BlazorWebAppMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; } = default!;
+    }
+}
